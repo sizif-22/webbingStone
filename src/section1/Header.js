13b099339963c5c663e1f3170c8a19b14dd36476
SiZiF <s1h9e7r4if@gmail.com>
2024-12-04 20:23:27 +0200

    finish section 1

--- src/section1/Header.js
+++ src/section1/Header.js
@@ -6,14 +6,14 @@ const Header = ()=>{
         <div className="flex px-10 py-2 items-center justify-between">
          <img height={48} width={150} src={logo} alt="webbingStone" loading="lazy"/>
          <div className="flex items-center gap-7 text-primary font-bold">
-            {/* <p className="text-secondary"><a href="#">Home</a></p>
+            <p className="text-secondary"><a href="#">Home</a></p>
             <a href="#">About Us</a>
             <a href="#">Services</a>
             <a href="#">Portfolio</a>
             <a href="#">Careers</a>
             <a href="#">Blog</a>
             <a href="#">Contact Us</a>
-            <a href="#">FAQ</a> */}
+            <a href="#">FAQ</a>
             <IoSearch className=" text-2xl"/>
          </div>
          
