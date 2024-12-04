851f926c2aef815401b2ac541585e9eae280ba73
SiZiF <s1h9e7r4if@gmail.com>
2024-12-04 20:54:37 +0200

    test section 1

--- src/section1/Section1.js
+++ src/section1/Section1.js
@@ -1,3 +1,4 @@
+import Body from "./Body";
 import Header from "./Header";
 import PreHeader from "./PreHeader";
 const Section1 = ()=>{
@@ -7,13 +8,7 @@ const Section1 = ()=>{
             <hr className="border-primary"/>
             <Header/>
             <hr className=" border-[#00000025]"/>
-            <div className="grid grid-cols-2">
-                <div>
-                    <h1>Accelerate Your Growth: Unleash the Power of Digital Marketing</h1>
-                    <p>WebbingStone is a digital marketing agency founded in 2015 and based in Cairo, Egypt, with international reach extending to the USA, UAE, Canada, and beyond. We offer a wide range of services, including social media marketing, web development, mobile app development, graphic design, SEO, and branding solutions. The company has worked with notable clients, such as Fujifilm and the Ministry of Tourism in Egypt, showcasing our ability to handle diverse industries.</p>
-                </div>
-                <div></div>
-            </div>
+            <Body/>
         </div>
     );
 
