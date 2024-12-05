5a61143f2a2e7b67fc07a8b173a49fda6100f242
SiZiF <s1h9e7r4if@gmail.com>
2024-12-05 04:54:07 +0200

    create a lazy load function

--- src/section1/Body.js
+++ src/section1/Body.js
@@ -1,4 +1,26 @@
+import React , {useRef, useEffect} from "react";
 const Body = () => {
+    const blurRef = useRef();
+    const imgRef = useRef();
+    useEffect(() => {
+        function loaded() {
+          // Add className "loaded" for the element blurRef
+          blurRef.current.classList.add("loaded");
+        }
+    
+        if (imgRef.current.complete) {
+          // If image is already loaded, add the loaded class immediately
+          blurRef.current.classList.add("loaded");
+        } else {
+          // Add event listener for load event if image is not yet complete
+          imgRef.current.addEventListener("load", loaded);
+    
+          // Cleanup function to remove event listener
+          return () => {
+            imgRef.current.removeEventListener("load", loaded);
+          };
+        }
+      }, []);
     return (
       <div className="grid grid-cols-2">
         {/* Left Section */}
@@ -18,19 +40,18 @@ const Body = () => {
   
         {/* Right Section */}
         <div className="items-center flex p-4 justify-center flex-col h-screen gap-4">
-          <img
-            src="https://webbingstone.com/wp-content/uploads/optimized_image.webp"
-            onError={(e) => {
-              e.target.onerror = null;
-              e.target.src =
-                "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png";
-            }}
+          <div ref={blurRef} className="blur-load h-[270px] w-[480px]">
+            <img
+            ref={imgRef}
+            src="https://webbingstone.com/wp-content/uploads/2024/05/webbingstone_video_May24-C2.webp"
+            loading="lazy"
             alt="webbingStone video"
             height={270}
             width={480}
-            loading="lazy"
-            className="object-contain"
+            className="object-contain the-gif h-[270px]"
           />
+          </div>
+          
         </div>
       </div>
     );
